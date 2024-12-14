4402daaa787d984d615ae8a21ed4d451cba78073
adanisantana <mariano.dani@hotmail.com>
2024-12-14 17:23:47 -0300

    alteracoes

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 	public Optional<Postagem> findByTitulo(String titulo);
